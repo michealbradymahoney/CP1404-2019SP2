21e9fb2c226d20e24e0d190d091da7309e25c9db
Micheal Brady-Mahoney <micheal.bradymahoney@my.jcu.edu.au>
2019-08-11 16:59:40 +1000

    Part two of files - read name from file

--- Practicals/prac_02/files.py
+++ Practicals/prac_02/files.py
@@ -5,4 +5,8 @@ name = input("What is your name?")
 out_file.write(name)
 out_file.close()
 
+in_file = open(OUTPUT_FILE, 'r')
+read_name = in_file.read()
+print("Your name is", name)
+in_file.close()
 
