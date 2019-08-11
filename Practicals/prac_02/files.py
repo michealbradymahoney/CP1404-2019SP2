ef28a55d745097d15691bf3108dcd2650f22e7a5
Micheal Brady-Mahoney <micheal.bradymahoney@my.jcu.edu.au>
2019-08-11 17:14:55 +1000

    Part 3 and 4 of files - read numbers from file and enumerate

--- Practicals/prac_02/files.py
+++ Practicals/prac_02/files.py
@@ -1,12 +1,17 @@
-OUTPUT_FILE = "name.txt"
-
-out_file = open(OUTPUT_FILE, 'w')
+out_file = open("name.txt", 'w')
 name = input("What is your name?")
 out_file.write(name)
 out_file.close()
 
-in_file = open(OUTPUT_FILE, 'r')
+in_file = open("name.txt", 'r')
 read_name = in_file.read()
 print("Your name is", name)
 in_file.close()
 
+file = open("numbers.txt")
+total = 0
+for line in file:
+    number = int(line)
+    total = total + number
+print(total)
+file.close()
