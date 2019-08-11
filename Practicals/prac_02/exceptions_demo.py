2967fba960b260cedc2292d1b5e268fd494d168d
Micheal Brady-Mahoney <micheal.bradymahoney@my.jcu.edu.au>
2019-08-11 16:43:32 +1000

    Answered questions and added error loop

--- Practicals/prac_02/exceptions_demo.py
+++ Practicals/prac_02/exceptions_demo.py
@@ -6,13 +6,21 @@ Answer the following questions:
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
 """
 
-try:
-    numerator = int(input("Enter the numerator: "))
-    denominator = int(input("Enter the denominator: "))
-    fraction = numerator / denominator
-    print(fraction)
-except ValueError:
-    print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
-print("Finished.")
+while True:
+    try:
+        numerator = int(input("Enter the numerator: "))
+        denominator = int(input("Enter the denominator: "))
+        fraction = numerator / denominator
+        print(fraction)
+    except ValueError:
+        print("Numerator and denominator must be valid numbers!")
+    except ZeroDivisionError:
+        print("Cannot divide by zero!")
+    print("Finished.")
+
+# Questions
+#
+# When will a ValueError occur? When they are not a number
+# When will a ZeroDivisionError occur? When trying to divide by zero
+# Could you change the code to avoid the possibility of a ZeroDivisionError? You can add while True
+# If you could figure out the answer to question 3, then make this change now.
