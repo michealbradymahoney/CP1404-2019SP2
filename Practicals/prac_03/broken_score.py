b6d27b82d049720ecf1ede48d6343e8120ac2a02
Micheal Brady-Mahoney <micheal.bradymahoney@my.jcu.edu.au>
2019-08-18 14:46:03 +1000

    fixed broken score checker and made 2 functions

--- Practicals/prac_03/broken_score.py
+++ Practicals/prac_03/broken_score.py
@@ -3,15 +3,24 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-else:
-    if score > 100:
+
+def main():
+    score = float(input("Enter score: "))
+    score_checker(score)
+
+
+def score_checker(score):
+    if score < 0:
         print("Invalid score")
-    if score > 50:
-        print("Passable")
-    if score > 90:
-        print("Excellent")
-if score < 50:
-    print("Bad")
+    else:
+        if score > 100:
+            print("Invalid score")
+        elif score > 90:
+            print("Excellent")
+        elif score > 50:
+            print("Passable")
+    if score < 50:
+        print("Bad")
+
+
+main()
