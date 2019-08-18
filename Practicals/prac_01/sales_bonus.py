4792fef34390cd5969b6a4619084c7cd2f35a8dc
Micheal Brady-Mahoney <micheal.bradymahoney@my.jcu.edu.au>
2019-08-18 17:33:53 +1000

    added instructions for the exercises to make sure it is completed correctly

--- Practicals/prac_01/sales_bonus.py
+++ Practicals/prac_01/sales_bonus.py
@@ -4,6 +4,19 @@ If sales are under $1,000, the user gets a 10% bonus.
 If sales are $1,000 or over, the bonus is 15%.
 """
 
+# Now write the Python code to complete the program according to that docstring.
+# The first line might look like:
+# sales = float(input("Enter sales: $"))
+# Run and test the code with a few different values to verify that it works.
+# Whenever you are testing code, you should not just use random values but values that you know the expected
+# output for and that test all paths of execution, including the boundary or edge case.
+# So, for this program we could use the following (we're only interested in the values, not the format):
+#
+# Test Input	Expected Output
+# 500	             50
+# 2000	            300
+# 1000 (edge case)	150
+
 sales = float(input("Enter sales: $"))
 if sales < 1000:
     bonus = sales * 0.1
