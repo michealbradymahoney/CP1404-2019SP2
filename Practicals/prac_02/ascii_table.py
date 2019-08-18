83a28301fce4d015d135e4f31c9f2edab555b679
Micheal Brady-Mahoney <micheal.bradymahoney@my.jcu.edu.au>
2019-08-18 22:07:04 +1000

    works out ascii value for character

---
+++ Practicals/prac_02/ascii_table.py
@@ -0,0 +1,5 @@
+# Start new file, ascii_table.py, and write code for this program. Remember that you can use the ord() and chr()
+# functions to convert characters to ASCII integer values and vice versa.
+
+char = input("Enter a character: ")
+print("The ASCII code for {} is {}".format(char, ord(char)))
