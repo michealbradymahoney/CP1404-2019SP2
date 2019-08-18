6b2d1e3b8df3483c0292a6e9e88513ce1752eb9f
Micheal Brady-Mahoney <micheal.bradymahoney@my.jcu.edu.au>
2019-08-18 23:31:52 +1000

    made main function

--- Practicals/prac_03/password_check.py
+++ Practicals/prac_03/password_check.py
@@ -1,9 +1,25 @@
 """
 Micheal Brady-Mahoney
 """
+# MINIMUM_LENGTH = 5
+#
+# password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
+# while len(password) < MINIMUM_LENGTH:
+#     password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
+# print('*' * len(password))
+
+# Move all of the code inside a main() function and call main() at the bottom.
+# Note: if you don't have a main function, the refactoring below will use global variables.
+# So, it's an important first step to use main before adding other functions.
+
 MINIMUM_LENGTH = 5
 
-password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
-while len(password) < MINIMUM_LENGTH:
+
+def main():
     password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
-print('*' * len(password))
+    while len(password) < MINIMUM_LENGTH:
+        password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
+    print('*' * len(password))
+
+
+main()
