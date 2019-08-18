5c127e5d161976b78e78f5cba9bd2bc1de69f60a
Micheal Brady-Mahoney <micheal.bradymahoney@my.jcu.edu.au>
2019-08-18 16:20:26 +1000

    updated password checker

--- Practicals/prac_03/password_checker.py
+++ Practicals/prac_03/password_checker.py
@@ -2,47 +2,24 @@
 CP1404/CP5632 - Practical
 Password checker "skeleton" code to help you get started
 """
-
-MIN_LENGTH = 2
-MAX_LENGTH = 6
-SPECIAL_CHARS_REQUIRED = True
-SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]\<>?{}|"
+MINIMUM_LENGTH = 4
 
 
 def main():
-    print("Please enter a valid password")
-    print("Your password must be between", MIN_LENGTH, "and", MAX_LENGTH, "characters, and contain:")
-    print("\t1 or more uppercase characters")
-    print("\t1 or more lowercase characters")
-    print("\t1 or more numbers")
-    if SPECIAL_CHARS_REQUIRED:
-        print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
-    password = input("> ")
-    while not is_valid_password(password):
-        print("Invalid password!")
-        password = input("> ")
-    print("Your " + str(len(password)) + " character password is valid: " + password)
+    password = get_password(MINIMUM_LENGTH)
+    print_asterisks(password)
 
 
-def is_valid_password(password):
-    count_lower = 0
-    count_upper = 0
-    count_digit = 0
-    count_special = 0
-    for char in password:
-        if char.islower():
-            count_lower += 1
-        elif char.isupper():
-            count_upper += 1
-        elif char.isdigit():
-            count_digit += 1
-        elif char in SPECIAL_CHARACTERS:
-            count_special += 1
+def get_password(minimum_length):
+    password = input("Enter password of at least {} characters: ".format(minimum_length))
+    while len(password) < minimum_length:
+        print("Password too short")
+        password = input("Enter password of at least {} characters: ".format(minimum_length))
+    return password
 
-    if count_lower == 0 or count_upper == 0 or count_digit == 0 or (count_special == 0 and SPECIAL_CHARS_REQUIRED):
-        return False
-    else:
-        return True
+
+def print_asterisks(sequence):
+    print('*' * len(sequence))
 
 
 main()
