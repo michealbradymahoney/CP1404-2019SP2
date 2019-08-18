5468c8cc07d865addbb323c7aba35d007cda2c44
Micheal Brady-Mahoney <micheal.bradymahoney@my.jcu.edu.au>
2019-08-18 17:35:54 +1000

    added instructions for exercise to make sure it is completed correctly

--- Practicals/prac_01/broken_score.py
+++ Practicals/prac_01/broken_score.py
@@ -2,6 +2,15 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+# Debugging:
+# Someone (it's not polite to say who) was trying to write a program to tell the user if their score is
+# invalid, bad, passable or excellent, but their code is in the "bad" category and doesn't work.
+# Rewrite the following programming attempt using the most efficient if-elif-else 'ladder' you can.
+# The code is available here at: broken_score.py
+# Remember to click Raw before copying and pasting so you get proper formatting!
+# The intention is that the score must be between 0 and 100 inclusive; 90 or more is excellent;
+# 50 or more is a pass; below 50 is bad.
+# Be very careful of your boundary conditions... and test!
 
 score = float(input("Enter score: "))
 if score < 0 or score > 100:
