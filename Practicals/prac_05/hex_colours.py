cc7c12d4b03b48163c97b4e8b2ca4e67df250e69
Micheal Brady-Mahoney <micheal.bradymahoney@my.jcu.edu.au>
2019-08-24 17:05:41 +1000

    colour lookup complete

--- Practicals/prac_05/hex_colours.py
+++ Practicals/prac_05/hex_colours.py
@@ -1,2 +1,10 @@
 HEX_COLOURS = {"red": "ff0000", "orange": "ffa500", "yellow": "ffff00", "green": "00ff00", "blue": "0000ff",
                "indigo": "4b0082", "violet": "ee82ee", "black": "000000", "white": "ffffff", "gray": "808080"}
+
+colour = input("Enter a colour: ").lower()
+while colour != "":
+    if colour in HEX_COLOURS:
+        print("{} is {}".format(colour, HEX_COLOURS[colour]))
+    else:
+        print("Invalid colour")
+    colour = input("Enter a colour: ").lower()
