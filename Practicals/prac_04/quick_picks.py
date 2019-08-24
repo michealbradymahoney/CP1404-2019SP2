c9ce42cf5052a12d299e1f9ad926cdd2c29087a2
Micheal Brady-Mahoney <micheal.bradymahoney@my.jcu.edu.au>
2019-08-24 14:01:32 +1000

    generating quick pick games TODO: sort numbers

---
+++ Practicals/prac_04/quick_picks.py
@@ -0,0 +1,16 @@
+import random
+
+MAX_NUMBER = 45
+MIN_NUMBER = 1
+NUMS_PER_GAME = 6
+
+num_games = int(input("How many quick picks"))
+
+for i in range(num_games):
+    picks = []
+    for j in range(NUMS_PER_GAME):
+        number = random.randint(MIN_NUMBER, MAX_NUMBER)
+        while number in picks:
+            number = random.randint(MIN_NUMBER, MAX_NUMBER)
+        picks.append(number)
+    print(picks)
