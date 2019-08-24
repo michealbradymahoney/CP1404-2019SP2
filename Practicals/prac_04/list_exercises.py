53e040cd5a3b7697c189ada00008e789734680dc
Micheal Brady-Mahoney <micheal.bradymahoney@my.jcu.edu.au>
2019-08-24 13:17:31 +1000

    completed username checker with loop using list lookup

--- Practicals/prac_04/list_exercises.py
+++ Practicals/prac_04/list_exercises.py
@@ -25,3 +25,12 @@ print("The last number is", numbers[-1])
 print("The smallest number is", min(numbers))
 print("The largest number is", max(numbers))
 print("The average of the numbers is", sum(numbers) / len(numbers))
+
+# Woefully inadequate security checker
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+entered_username = input("Enter username: ")
+if entered_username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
