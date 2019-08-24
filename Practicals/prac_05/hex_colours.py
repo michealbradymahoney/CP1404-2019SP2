8ddc5e623229f932c7dc00d40509b60de00be923
Micheal Brady-Mahoney <micheal.bradymahoney@my.jcu.edu.au>
2019-08-24 16:57:42 +1000

    created colour dict

---
+++ Practicals/prac_05/hex_colours.py
@@ -0,0 +1,2 @@
+HEX_COLOURS = {"red": "ff0000", "orange": "ffa500", "yellow": "ffff00", "green": "00ff00", "blue": "0000ff",
+               "indigo": "4b0082", "violet": "ee82ee", "black": "000000", "white": "ffffff", "gray": "808080"}
