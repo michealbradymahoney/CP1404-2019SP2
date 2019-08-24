822bbde0527f9ca373b177e9deddd305b019b4be
Micheal Brady-Mahoney <micheal.bradymahoney@my.jcu.edu.au>
2019-08-24 16:37:53 +1000

    prints states formatted

--- Practicals/prac_05/state_names.py
+++ Practicals/prac_05/state_names.py
@@ -9,10 +9,12 @@ STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Te
                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 # print(STATE_NAMES)
 
-state = input("Enter short state: ").upper()
-while state != "":
-    if state in STATE_NAMES:
-        print(state, "is", STATE_NAMES[state])
-    else:
-        print("Invalid short state")
-    state = input("Enter short state: ").upper()
+# state = input("Enter short state: ").upper()
+# while state != "":
+#     if state in STATE_NAMES:
+#         print(state, "is", STATE_NAMES[state])
+#     else:
+#         print("Invalid short state")
+#     state = input("Enter short state: ").upper()
+
+print("\n".join("{}\t{}".format(short_state, long_state) for short_state, long_state in STATE_NAMES.items()))
