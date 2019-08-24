e9d8416eebaa88c8a8d8fa1e67437b7a8081e184
Micheal Brady-Mahoney <micheal.bradymahoney@my.jcu.edu.au>
2019-08-24 13:03:31 +1000

    added in questions for clarity

--- Practicals/prac_04/lists_warmup.py
+++ Practicals/prac_04/lists_warmup.py
@@ -1,5 +1,6 @@
 numbers = [3, 1, 4, 1, 5, 9, 2]
 
+# What values do the following expressions have?
 # numbers[0] = 3
 # numbers[-1] = 2
 # numbers[3] = 1
@@ -9,6 +10,13 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # 7 in numbers = False
 # "3" in numbers = False
 # numbers + [6, 5, 3] = 3, 1, 4, 1, 5, 9, 2, 6, 5, 3
+
+# Write Python expressions (in your Python file) to achieve the following:
+#
+# Change the first element of numbers to "ten"
+# Change the last element of numbers to 1
+# Get all the elements from numbers except the first two
+# Check if 9 is an element of numbers
 
 # numbers.insert(0, "ten")
 # numbers[-1] = 1
