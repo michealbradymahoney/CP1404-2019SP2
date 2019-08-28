9d2a21b948d1a887492708a267aeb95b3fb523a4
Micheal Brady-Mahoney <micheal.bradymahoney@my.jcu.edu.au>
2019-08-28 12:20:29 +1000

    Extension Work: asks user for name and dob 5 times and splits the dob into list using split regular expression

--- Practicals/prac_05/list_to_dict.py
+++ Practicals/prac_05/list_to_dict.py
@@ -2,12 +2,15 @@
 # date-of-birth details for 5 people, and have it display their individual ages. Hint: you can split() a string like
 # "12/4/1999", as we did in the lecture last week.
 
-date_seperator = ('/', ' ', '-', '.', ',')
+import re
+
+date_separator = ('/', ' ', '-', '.', ',')
 name_birth_dict = dict()
 
-name = input("Enter a name: ")
-date_of_birth = input("Enter date of birth: ")
-
-name_birth_dict[name] = date_of_birth
+for i in range(0, 5):
+    name = input("Enter a name: ")
+    date_of_birth = input("Enter date of birth: ")
+    date_of_birth = re.split("[%s]" % ("".join(date_separator)), date_of_birth)
+    name_birth_dict[name] = date_of_birth
 
 print(name_birth_dict)
