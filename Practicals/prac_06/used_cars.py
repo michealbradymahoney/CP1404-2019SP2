294505bb2dafa369479e70a735ccba533ef8c745
Micheal Brady-Mahoney <micheal.bradymahoney@my.jcu.edu.au>
2019-09-24 11:37:30 +1000

    Walkthrough modifications complete Added limo object Added name to class

--- Practicals/prac_06/used_cars.py
+++ Practicals/prac_06/used_cars.py
@@ -6,14 +6,22 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("my_car", 180)
     my_car.drive(30)
+    print("name = ", my_car.name)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
     print(my_car)
 
-    print("Car {}, {}".format(my_car.fuel, my_car.odometer))
-    print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+    print("Car {}, {}, {}".format(my_car.name, my_car.fuel, my_car.odometer))
+    print("Car {self.name} {self.fuel}, {self.odometer}".format(self=my_car))
+
+    limo = Car("limo", 100)
+    limo.add_fuel(20)
+    print(limo.name)
+    print(limo.fuel)
+    limo.drive(115)
+    print(limo.odometer)
 
 
 main()
